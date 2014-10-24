019778ef3e6297b6003f8f86da65ad28e3bd823d
Eugene Kulesha <kulesha@gmail.com>
2014-10-24 13:24:55 +0100

    now we parse the cvs files to find location of transposons

--- js/main.js
+++ js/main.js
@@ -2,12 +2,123 @@ function sleep(millis, callback) {
     setTimeout(function() { callback(); }, millis);
 }
 
+var labels = {
+    'flank5' : "5' Flank",
+    'utr5' : "5' UTR",
+    'gap5' : "5' Gap",
+    'cds' : "CDS",
+    'gap3' : "3' Gap",
+    'utr3' : "3' UTR",
+    'flank3' : "3' Flank"
+};
+
+function getExon(pos, t) {
+    var i = 0;
+    if (t.strand > 0) {
+    for (var j in t.Exon) {
+        if (pos < t.Exon[j].start) {
+            return i ? "Intron " + i : ''; // -i
+            return -i;
+        } else {
+            if (pos <= t.Exon[j].end) {
+                //return i+1;
+                return "Exon " + (i + 1);
+            }
+        }
+        i++;
+    }
+    } else {
+        for (var j in t.Exon) {
+            if (pos > t.Exon[j].end) {
+                //return -i;
+                return i ? "Intron " + i : ''; // -i
+            } else {
+                if (pos >= t.Exon[j].start) {
+                  //  return i+1;
+                    return "Exon " + (i + 1);
+                }
+            }
+            i++;
+        }
+
+    }
+    return '';
+    return 0;
+}
+
+function getPosition( pos, t) {
+    var cl = getClass(pos, t);
+    return labels[ cl ];
+}
+
+function getClass( pos, t) {
+    if (t.ori > 0) {
+    if (pos < t.gs) {
+        return "flank5";
+    } else {
+        if (pos <= t.utr5e) {
+            return "utr5";
+        } else {
+            if (pos < t.cdss ) {
+                return "gap5";                                        
+            } else {
+                if (pos <= t.cdse ) {
+                    return 'cds';
+                } else {
+                    if (pos < t.utr3s) {
+                        return "gap3";
+                    } else {
+                        if (pos <= t.utr3e) {
+                            return "utr3";
+                        } else {
+                            return "flank3";
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return "Check the data";
+    } else {
+        if (pos < t.gs) {
+            return "flank3";
+        } else {
+            if (pos <= t.utr3e) {
+                return "utr3";
+            } else {
+                
+            if (pos < t.cdss ) {
+                return "gap3";                                        
+            } else {
+                if (pos <= t.cdse ) {
+                    return 'cds';
+                } else {
+                    if (pos < t.utr5s) {
+                        return "gap5";
+                    } else {
+                        if (pos <= t.utr5e) {
+                            return "utr5";
+                        } else {
+                            return "flank5";
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return "Check the data";
+    }
+}
+
+
 // Define the app, ngSanitize is needed to enable passing plain html into ng-repeat
 var myApp = angular.module('geneInfoApp', ['ngSanitize', 'ngCsv']);
 
 myApp.controller('geneInfoCtrl', ['$scope', '$http', '$sce', '$location', '$anchorScroll', '$window', function ($scope, $http, $sce, $location, $anchorScroll, $window) { 
      $scope.formInfo = {
         gene: "", //"ARSE\nBRCA2", 
+        //fname: "http://www.ebi.ac.uk/~ek/all_1043.csv",
+         fname: "/data/test.csv",
         width: 100, 
         coding: false, 
         restServer: 'http://grch37.rest.ensembl.org',
@@ -63,6 +174,32 @@ myApp.controller('geneInfoCtrl', ['$scope', '$http', '$sce', '$location', '$anch
         return ['Gene Name', 'Region','Start', 'End', 'Strand', 'Transcript', 'Tr.Start', 'Tr.End', '5UTR Start', '5UTR End', , 'CDS Start', 'CDS End', '3UTR Start', '3UTR End'];
     };
     
+    $scope.getHeader2 = function () {
+        return ['Sample','Region','PositiveBrk', 'PositiveLoc','PositiveExon','NegativeBrk', 'NegativeLoc', 'NegativeExon', 'Gene'];
+    };
+   
+    $scope.getResults2 = function() {
+    var ar = new Array();
+    for (var i in $scope.transposons) {
+        var e = $scope.transposons[i];
+        if (e) {
+            var r = {sample: e.id,
+                    chr: e.r,
+                 start: e.s,
+                 startP: e.scol,
+                 startE: e.posS,
+                 end: e.e,
+                 endP: e.ecol,
+                 endE: e.posE, 
+                  gene: e.g
+                };
+            ar.push(r);
+            }
+    }
+        
+       return ar;
+   };
+   
     this.getGene = function(gene) {
         var o = { display_name: gene };
         $scope.message = " Looking for " + gene;
@@ -154,8 +291,107 @@ myApp.controller('geneInfoCtrl', ['$scope', '$http', '$sce', '$location', '$anch
         });
 
 
-    }
-    
+    };
+
+    this.fetchGene = function(gene) {
+        var o = { display_name: gene };
+        $scope.message = " Looking for " + gene;
+        console.log(" Looking for " + gene);
+        $scope.total++;
+         // first we look for the gene
+        var url = $scope.formInfo.restServer + '/lookup/symbol/'+self.species+'/' + gene
+                + '?content-type=application/json;expand=1';
+
+        $http.get(url).success(function(data){
+            // hooray - we have found the gene
+            o = data;
+//            console.log(o);
+            o.gene_url = $scope.formInfo.eServer + '/'+self.species+'/Gene/Summary?g=' + data.id;
+            
+            // now let's get the sequence
+            for (var i in data.Transcript) {
+                var t = data.Transcript[i];
+                if (t.is_canonical === "1") {
+                    o.transcript = t;
+                    o.transcript_url = $scope.formInfo.eServer + '/'+self.species+'/Transcript/Summary?t=' + t.id;
+                    var cds = $scope.formInfo.restServer + '/map/cds/'+t.id+'/1..3000000?content-type=application/json';
+                    var t1 = t;
+                    if (t1.Translation) {
+                    
+                    $http.get(cds).success(function(data){
+                        var f = data.mappings[0];
+                        data.mappings.pop();
+                        
+                        var l = data.mappings.pop();
+                        if (o.strand > 0) {
+                            if (t1.start == t1.Translation.start) {
+                                o.transcript.start5 = 0;
+                                o.transcript.end5 = 0;                                
+                            } else {
+                                o.transcript.start5 = t1.start;
+                                o.transcript.end5 = t1.Translation.start-1;
+                            }
+                            
+                            if (t1.end == t1.Translation.end) {
+                                o.transcript.start3 = 0;
+                                o.transcript.end3 = 0;                                
+                            } else {
+                                o.transcript.start3 = t1.Translation.end +1;
+                                o.transcript.end3 = t1.end;
+                            }
+                            
+                            o.transcript.startCds = t1.Translation.start;
+                            o.transcript.endCds = t1.Translation.end;
+
+                        } else {
+                            if (t1.end == t1.Translation.end) {
+                                o.transcript.start5 = 0;
+                                o.transcript.end5 = 0;
+                            
+                            } else {
+                                o.transcript.start5 = f.end +1;
+                                o.transcript.end5 = t1.end;
+                            
+                            }
+                            if (t1.start == t1.Translation.start) {
+                                o.transcript.start3 = 0;
+                                o.transcript.end3 = 0;
+                            } else {
+                                o.transcript.start3 = t1.start;
+                                o.transcript.end3 = t1.Translation.start-1;
+                            }
+                            o.transcript.startCds = t1.Translation.start;
+                            o.transcript.endCds = t1.Translation.end;
+
+                        }                        
+                    });
+                    } else {
+                        if (1) {
+                            o.transcript.start5 = 0;
+                            o.transcript.end5 = 0;
+                            o.transcript.start3 = 0;
+                            o.transcript.end3 = 0;
+                            o.transcript.Translation = {start: 0, end: 0};
+                            
+                        }
+
+                    }
+                    break;
+                }
+            }
+            $scope.gHash[ o.display_name ] = o;
+            $scope.valid++;
+        }).error(function(data, status, header, config){
+            if (status === 400) {
+                $scope.message = data.error;
+            }
+        //    o.seq_region_name = data.error;
+        //    $scope.results.push(o);
+        });
+
+
+    };
+
     this.find = function() {
         $scope.results = [];
         $scope.toFind = $scope.formInfo.gene.toUpperCase().split(/[\,\n\r]/);
@@ -186,6 +422,118 @@ myApp.controller('geneInfoCtrl', ['$scope', '$http', '$sce', '$location', '$anch
             $scope.message = 'Done';
             $scope.finished = 1;
         }
+    };
+
+    
+    this.calcPositions = function() {
+  //      console.log($scope.gHash);
+        for (var i in $scope.transposons ) {
+                var t = $scope.transposons[i];
+                
+                if (t) {
+                    var g = $scope.gHash[ t.g ];
+//                    console.log(t.g);
+//                    console.log(g);
+                    
+                    if (g) {
+                        // find canonical transcript
+                        var tl = g.transcript;
+                        t.ori = tl.strand;         
+                        t.gs = tl.start;
+                        t.ge = tl.end;
+                        t.tid = tl.id;
+                        t.gr = tl.seq_region_name;
+                        t.utr3s = tl.start3;
+                        t.utr3e = tl.end3;
+                        t.utr5s = tl.start5;
+                        t.utr5e = tl.end5;
+                        t.cdss = tl.startCds;
+                        t.cdse = tl.endCds;
+                        
+                        t.spos = getPosition(t.s, t);
+                        t.epos = getPosition(t.e, t);
+                        
+                        t.scol = getClass(t.s, t);
+                        t.ecol = getClass(t.e, t);
+                        
+                        t.posS = getExon(t.s, tl);
+                        t.posE = getExon(t.e, tl);
+                        
+                        $scope.transposons[i] = t;                        
+                    }
+              //      console.log(t);
+                }
+            }
+            
+    //        console.log($scope.transposons);
+    };
+    this.fetch_genes = function( i ) {
+        if ($scope.toFind[i]) {
+            var gene = $scope.toFind[i].replace(/\s/g, '');
+            if (gene) {
+                self.fetchGene(gene);
+                i++;
+                if (i === $scope.toFind.length) {
+                    $scope.message = 'Done';
+                    $scope.finished = 1;
+                    this.calcPositions();                    
+                } else {
+                    setTimeout(function() { self.fetch_genes(i); }, 1000);
+                }
+            }
+        } else {
+            $scope.message = 'Done';
+            $scope.finished = 1;
+      //      console.log($scope.gHash);
+            
+            
+        }
+    };
+
+    this.reset = function() {
+        $scope.transposons = [];
+        $scope.results = [];
+    };
+    
+    this.getGeneData = function(gene) {
+        // first we look for the gene
+        var url = $scope.formInfo.restServer + '/lookup/symbol/'+self.species+'/' + gene
+                + '?content-type=application/json;expand=1';
+            
+        $http.get(url).success(function(data){
+        });
+    };
+    this.transposonSearch = function() {
+        var fname = $scope.formInfo.fname;
+        console.log(fname);
+        self.reset();
+        $http.get(fname).success(function(data){
+            var entries = data.split(/[\n]/).filter(function(n) {return n != undefined });
+        //    console.log(entries);
+            
+            $scope.genes = {};
+            $scope.transposons = entries.map(function(item) {
+                var edata = item.split(/,/);
+                if (edata[0]) {
+                    var g = edata[6];
+                    if (g && g.match(/[A-Z|a-z]/) && g !== "Gene") {
+                        $scope.genes[ g ] = { };                        
+                           return { id: edata[0], r: edata[1], s : edata[2], e: edata[3], g: edata[6] };
+                    }
+                 
+                }
+            });
+            
+          //  console.log($scope.genes);
+            
+            $scope.toFind = Object.keys($scope.genes).sort();
+            $scope.toFind.push('BRCA2'); // tmp fix 
+            $scope.gHash = {};
+            $scope.finished = 0;
+            self.fetch_genes(0);
+        });
+        
+        
     };
     
       // function that will be called on form submit
@@ -232,11 +580,16 @@ myApp.controller('geneInfoCtrl', ['$scope', '$http', '$sce', '$location', '$anch
 
                             $scope.geneInfo.transcript.start3 = t.Translation.end +1;
                             $scope.geneInfo.transcript.end3 = t1.end;
+                            
+                            $scope.geneInfo.transcript.startCds = t.Translation.start;
+                            $scope.geneInfo.transcript.endCds = t.Translation.end;
                         } else {
                             $scope.geneInfo.transcript.start5 = f.end +1;
                             $scope.geneInfo.transcript.end5 = t1.end;
                             $scope.geneInfo.transcript.start3 = t1.start;
                             $scope.geneInfo.transcript.end3 = t.Translation.start-1;
+                            $scope.geneInfo.transcript.startCds = t.Translation.start;
+                            $scope.geneInfo.transcript.endCds = t.Translation.end;
 
                         }
                         
